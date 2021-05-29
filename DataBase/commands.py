c4d72a1f78bdb9289fb7d99c7a394a6984f7b7fe
iparinile <poselennov.il@ya.ru>
2021-05-29 21:07:26 +0500

    Добавление

--- DataBase/commands.py
+++ DataBase/commands.py
@@ -1,3 +1,7 @@
 def add_user(user_id, cursor, db):
     cursor.execute(f"INSERT OR IGNORE INTO Users (user_id) VALUES ({user_id})")
     db.commit()
+
+def add_post(group_domain, post_id,post_text,post_date, cursor,db):
+    cursor.execute(f"INSERT INTO Posts(group_domain, post_id,post_text,post_date) VALUES ({group_domain},{post_id},{post_text},{post_date})")
+    db.commit()
