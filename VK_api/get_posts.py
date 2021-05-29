ff59b429d5d945f41dcae3bdae053d94ad7f78dc
iparinile <poselennov.il@ya.ru>
2021-05-29 20:15:07 +0500

    массив доменов

--- VK_api/get_posts.py
+++ VK_api/get_posts.py
@@ -1,1 +1,13 @@
 import vk_api
+
+domains = ['bu_truba_zovet', 'translom_pererabotka', 'prodam_trubu', 'transfer1tube', 'tryba_by_vosstanovlenay', 'public116166768', 'club17083336', 'metalopt', 'nelikvid', 'onamazov2014', 'id324213859', 'neewtruba']
+
+
+def get_post(domain_of_group, data_of_last_post, count_of_posts):
+  token='1c7c90141c7c90141c7c9014d11c0b69d811c7c1c7c90147cd753b4ab6c4777f6d7df33',
+  api_version='5.131'
+
+
+
+
+
