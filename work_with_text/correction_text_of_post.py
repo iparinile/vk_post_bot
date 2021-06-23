32c505f80e7e36f6537444c81a176c71675b9954
nastya <nastya.vergeeva@mail.ru>
2021-06-23 23:01:07 +0500

    edit function "correction_text_of_post"

--- work_with_text/correction_text_of_post.py
+++ work_with_text/correction_text_of_post.py
@@ -21,6 +21,34 @@ def correction_text_of_post(text: str) -> str:
                             continue
                         except ValueError:
                             pass
+                else:
+                    if text[finding - 1] == ' ' and text[finding + 1] != ' ':
+                        try:
+                            int(text[finding - 2])
+                            text = text[:finding - 1] + 'х' + text[finding + 1:]
+                            start_on = finding + 1
+                        except ValueError:
+                            try:
+                                int(text[finding - 3])
+                                text = text[:finding - 2] + 'х' + text[finding + 1:]
+                                start_on = finding + 1
+                                continue
+                            except ValueError:
+                                pass
+                    elif text[finding - 1] != ' ' and text[finding + 1] == ' ':
+                        try:
+                            int(text[finding - 1])
+                            text = text[:finding] + 'х' + text[finding + 2:]
+                            start_on = finding + 1
+                        except ValueError:
+                            try:
+                                int(text[finding - 3])
+                                text = text[:finding - 1] + 'х' + text[finding + 1:]
+                                start_on = finding + 1
+                                continue
+                            except ValueError:
+                                pass
+
             except IndexError:
                 pass
             try:
@@ -33,7 +61,6 @@ def correction_text_of_post(text: str) -> str:
 
 
 if __name__ == '__main__':
-    data = 'х продам трубу 1420*17,5-18,7 отличного качества , цена в безнале 60,000 с ндс. продам трубу 1220*12 с ' \
-           'размером 120м x 240м х'
+    data = '325 х9 - 61000 состояние 325х 8-10 - 54000 325 x 8 х 9 х 10 ц/т - 50000 руб/тн фаска орбита (Объём)'
 
     print(correction_text_of_post(data))
